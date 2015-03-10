2dabc98fdbf958857df414aad3c97820ede51181
Alessio <alessio.falzarano@gmail.com>
2015-03-10 17:22:46 +0100

    first commit

--- template3/js/contact_script.js
+++ template3/js/contact_script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+jQuery(document).ready(function(){
     $(".um_form").submit(function(){
         var $var = $('[name="umbheadfld_Name"]').val();
         alert($var);

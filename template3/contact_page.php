edd38b9955e86403e49eab3a56d5823a06f8df86
Alessio <alessio.falzarano@gmail.com>
2015-03-10 17:47:17 +0100

    first commit

--- template3/contact_page.php
+++ template3/contact_page.php
@@ -192,7 +192,7 @@
                     <textarea data-required="1" name="umbheadfld_Message" id="umbheadfld_Message" placeholder="your message goes here..."></textarea>
                 </div>
                 <div class="buttons">
-                    <input type="submit" onclick="alert('prova')" value="Send Message"/>
+                    <input type="submit"  value="Send Message"/>
                     <input type="reset" value="Reset">
                 </div>
             </form>
